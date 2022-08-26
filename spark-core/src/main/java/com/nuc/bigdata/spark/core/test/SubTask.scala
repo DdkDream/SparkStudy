34acf381bf44abc982463031e84428b832db7d31
zrm <643910751@qq.com>
2022-08-26 21:02:20 +0800

    分布式计算模拟

---
+++ spark-core/src/main/java/com/nuc/bigdata/spark/core/test/SubTask.scala
@@ -0,0 +1,13 @@
+package com.nuc.bigdata.spark.core.test
+
+class SubTask extends Serializable {
+  var datas: List[Int] = _
+  var logic: (Int) => Int = _
+
+  // 计算
+  def compute() = {
+    datas.map(logic)
+  }
+
+
+}
---
+++ spark-core/src/main/java/com/nuc/bigdata/spark/core/test/Task.scala
@@ -0,0 +1,15 @@
+package com.nuc.bigdata.spark.core.test
+
+class Task extends Serializable {
+
+  val datas = List(1, 2, 3, 4)
+
+//  val logic = ( num: Int ) => { num * 2 }
+  val logic: (Int) => Int = _ * 2
+
+  // 计算
+  def compute() = {
+    datas.map(logic)
+  }
+
+}
