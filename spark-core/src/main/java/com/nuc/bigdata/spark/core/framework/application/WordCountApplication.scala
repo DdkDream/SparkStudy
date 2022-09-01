f97003dd6a5e999255b3973f120bb3fefe583c04
zrm <643910751@qq.com>
2022-09-01 15:13:37 +0800

    SparkCore 案例实操

--- spark-core/src/main/java/com/nuc/bigdata/spark/core/framework/application/WordCountApplication.scala
+++ spark-core/src/main/java/com/nuc/bigdata/spark/core/framework/application/WordCountApplication.scala
@@ -1,8 +1,12 @@
 package com.nuc.bigdata.spark.core.framework.application
 
 import com.nuc.bigdata.spark.core.framework.controller.WordCountController
+import org.apache.spark.SparkConf
 
 object WordCountApplication extends App {
+
+    val sparConf = new SparkConf().setMaster()
+
 
     val controller = new WordCountController()
 }
